e9b1ca5ab71c137d0a3e648b25ed4ccd5473a71b
j-chimienti <jchimien@mail.usf.edu>
2017-12-22 11:57:01 -0500

    iris summary

--- src/winequality.py
+++ src/winequality.py
@@ -6,22 +6,20 @@ url_base = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-qualit
 
 
 def classify_wine(wine='red'):
-  url = url_base.format(wine)
+    url = url_base.format(wine)
 
-  data = pd.read_csv(url, header=0, sep=';')
+    data = pd.read_csv(url, header=0, sep=';')
 
-  X = data.iloc[:, :-1].values
+    X = data.iloc[:, :-1].values
 
-  y = data.iloc[:, -1].values
+    y = data.iloc[:, -1].values
 
-  columns = [*data.columns]
+    columns = data.columns
 
-  print('')
-  print('# {} Wine Quality'.format(wine.capitalize()))
+    print('')
+    print('# {} Wine Quality'.format(wine.capitalize()))
 
-  print('')
-
-  Classify(X, y, standardizeData=True, columns=columns)
+    print('')
 
 
 classify_wine('red')
